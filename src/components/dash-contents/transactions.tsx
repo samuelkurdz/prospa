4de2570443b5be677bda550b43e77d32acd5b0cc
Samuel Adebayo <sambayo28@gmail.com>
2021-12-18 19:23:43 +0100

    transaction section

--- src/components/dash-contents/transactions.tsx
+++ src/components/dash-contents/transactions.tsx
@@ -2,11 +2,100 @@ import Button from "../Button";
 
 function Transactions() {
   return <div className="bg-white p-7 rounded-md shadow-md mt-8">
-    <header className="flex justify-between items-center"> 
+    <header className="flex justify-between items-center mb-7"> 
       <p className="font-semibold">Recent transactions</p>
       <Button secondary small>See all</Button>
     </header>
 
+    <div>
+      <section className="flex justify-between items-baseline">
+        <div className="flex items-center gap-4">
+          <div className="bg-[#F4F8FB] w-10 h-10 flex items-center justify-center rounded-full">
+          <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fillRule="evenodd" clipRule="evenodd" d="M0 4.08994L8 0.384216L16 4.08994V5.57223H0V4.08994ZM1.6 7.05452H4V12.2425H1.6V7.05452ZM6.8 12.2425V7.05452H9.2V12.2425H6.8ZM16 15.9482V13.7248H0V15.9482H16ZM12 7.05452H14.4V12.2425H12V7.05452Z" fill="#8397AB"/>
+          </svg>
+
+          </div>
+          <div>
+            <p className="font-semibold">Transfer Fee</p>
+            <p className="font-medium text-subText text-sm">12:49 AM</p>
+          </div>
+        </div>
+        <div className="font-medium">-N145.90</div>
+      </section>
+
+      <div className="w-full h-px bg-[#E6EFF8] my-6"></div>
+
+      <section className="flex justify-between items-baseline">
+        <div className="flex items-center gap-4">
+          <div className="bg-[#F4F8FB] w-10 h-10 flex items-center justify-center rounded-full">
+          <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fillRule="evenodd" clipRule="evenodd" d="M0 4.08994L8 0.384216L16 4.08994V5.57223H0V4.08994ZM1.6 7.05452H4V12.2425H1.6V7.05452ZM6.8 12.2425V7.05452H9.2V12.2425H6.8ZM16 15.9482V13.7248H0V15.9482H16ZM12 7.05452H14.4V12.2425H12V7.05452Z" fill="#8397AB"/>
+          </svg>
+
+          </div>
+          <div>
+            <p className="font-semibold">Transfer Fee</p>
+            <p className="font-medium text-subText text-sm">12:49 AM</p>
+          </div>
+        </div>
+        <div className="font-medium">-N145.90</div>
+      </section>
+
+      <div className="w-full h-px bg-[#E6EFF8] my-6"></div>
+
+      <section className="flex justify-between items-baseline">
+        <div className="flex items-center gap-4">
+          <div className="bg-[#F4F8FB] w-10 h-10 flex items-center justify-center rounded-full">
+          <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fillRule="evenodd" clipRule="evenodd" d="M0 4.08994L8 0.384216L16 4.08994V5.57223H0V4.08994ZM1.6 7.05452H4V12.2425H1.6V7.05452ZM6.8 12.2425V7.05452H9.2V12.2425H6.8ZM16 15.9482V13.7248H0V15.9482H16ZM12 7.05452H14.4V12.2425H12V7.05452Z" fill="#8397AB"/>
+          </svg>
+
+          </div>
+          <div>
+            <p className="font-semibold">Transfer Fee</p>
+            <p className="font-medium text-subText text-sm">12:49 AM</p>
+          </div>
+        </div>
+        <div className="font-medium">-N145.90</div>
+      </section>
+
+      <div className="w-full h-px bg-[#E6EFF8] my-6"></div>
+
+      <section className="flex justify-between items-baseline">
+        <div className="flex items-center gap-4">
+          <div className="bg-[#F4F8FB] w-10 h-10 flex items-center justify-center rounded-full">
+          <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fillRule="evenodd" clipRule="evenodd" d="M0 4.08994L8 0.384216L16 4.08994V5.57223H0V4.08994ZM1.6 7.05452H4V12.2425H1.6V7.05452ZM6.8 12.2425V7.05452H9.2V12.2425H6.8ZM16 15.9482V13.7248H0V15.9482H16ZM12 7.05452H14.4V12.2425H12V7.05452Z" fill="#8397AB"/>
+          </svg>
+
+          </div>
+          <div>
+            <p className="font-semibold">Transfer Fee</p>
+            <p className="font-medium text-subText text-sm">12:49 AM</p>
+          </div>
+        </div>
+        <div className="font-medium">-N145.90</div>
+      </section>
+
+      <div className="w-full h-px bg-[#E6EFF8] my-6"></div>
+
+      <section className="flex justify-between items-baseline">
+        <div className="flex items-center gap-4">
+          <div className="bg-[#F4F8FB] w-10 h-10 flex items-center justify-center rounded-full">
+          <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fillRule="evenodd" clipRule="evenodd" d="M0 4.08994L8 0.384216L16 4.08994V5.57223H0V4.08994ZM1.6 7.05452H4V12.2425H1.6V7.05452ZM6.8 12.2425V7.05452H9.2V12.2425H6.8ZM16 15.9482V13.7248H0V15.9482H16ZM12 7.05452H14.4V12.2425H12V7.05452Z" fill="#8397AB"/>
+          </svg>
+
+          </div>
+          <div>
+            <p className="font-semibold">Transfer Fee</p>
+            <p className="font-medium text-subText text-sm">12:49 AM</p>
+          </div>
+        </div>
+        <div className="font-medium">-N145.90</div>
+      </section>
+    </div>
   </div>;
 }
 
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 
 import Aside from '../components/dashboard/aside';
 import Header from '../components/dashboard/header';
 import DashboardContent from '../components/dashboard/content';
 
+
+const dashContainer: CSSProperties = {
+  height: 'calc(100vh - 80px)',
+  overflowY: 'scroll'
+}
 const Dashboard = () => {
   return (
     <div className="grid grid-cols-5 h-screen">
       <Aside />
       <div className="col-span-4 bg-[#F4F8FB] h-full">
         <Header />
-        <section className="py-9 px-20">
+        <section className="py-9 px-20" style={dashContainer}>
           <DashboardContent />
         </section>
       </div>
