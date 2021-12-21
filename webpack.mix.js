8af75321b935a5ca941a687f4370c9120603d047
Samuel Adebayo <sambayo28@gmail.com>
2021-12-21 08:49:55 +0100

    remove unnecessary webpack file

--- webpack.mix.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-undef */
-const tailwindcss = require('tailwindcss');
-
-mix.sass('resources/assets/sass/app.scss', 'public/css')
-   .options({
-      processCssUrls: false,
-      postCss: [ tailwindcss('./tailwind.config.js') ],
-   });
